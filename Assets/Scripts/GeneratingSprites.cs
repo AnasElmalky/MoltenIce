5a859d239ea7dfc5aa6866d391105f68ed0b207a
Anas Ahmed <anas.elmalky1@gmail.com>
2020-01-22 14:37:01 +0200

    Revert "Revert "Making endless runner map""
    This reverts commit 15af164c3753ef9fdcc3d3acbb6aad25075a454f.

---
+++ Assets/Scripts/GeneratingSprites.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratingSprites : MonoBehaviour
+{
+    #region VARIABLES
+    [Header("Refernce Variables")]
+    [SerializeField]
+    GameObject cameraToDrawAfter;
+    [SerializeField]
+    GameObject spriteParent;
+
+    [Header("development variables")]
+    float spriteHeight;
+    #endregion
+
+    #region UNITY CALLBACKS
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject == cameraToDrawAfter)
+        {
+            Generate();
+        }
+    }
+    #endregion
+
+    #region PRIVATE METHODS
+    private void Generate()
+    {
+        GameObject currentSprite = Instantiate(this.gameObject, spriteParent.transform);
+
+        currentSprite.transform.position = new Vector3(
+            currentSprite.transform.position.x + currentSprite.GetComponent<SpriteRenderer>().size.x / 10,
+            currentSprite.transform.position.y,
+            currentSprite.transform.position.z
+            );
+    }
+    #endregion
+}
