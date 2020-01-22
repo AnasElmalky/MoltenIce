ad4f6056ecd1bd5bbe3dbbd0e93f8b69bf86cbe1
Anas Ahmed <anas.elmalky1@gmail.com>
2020-01-22 14:37:18 +0200

    Revert "Revert "Revert "Revert "Making endless runner map""""
    This reverts commit 57d0b9701e599ffa3bc0c6f10a87b05b09766d15.

---
+++ Assets/Scripts/OnObjectExitDestroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnObjectExitDestroy : MonoBehaviour
+{
+    #region VARIABLES
+    [Header("Refernce Variables")]
+    [SerializeField]
+    GameObject exitedObject;
+
+    [Header("development variables")]
+    float spriteHeight;
+    #endregion
+
+    #region UNITY CALLBACKS
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == exitedObject)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    #endregion
+}
